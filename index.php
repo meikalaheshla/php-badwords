0245b8cc28452f27cdabc49a1cbb8f88ca0bbead
meikalaheshla <fausto.boshoff@gmail.com>
2022-08-29 15:59:24 +0200

    added censured word with query

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php 
+
 $paragrafo = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
 echo '<pre>';
 var_dump($paragrafo);
-// strlen($paragrafo);
 echo '</pre>';
+$badword = $_GET['badword'];
+$new_paragrafo = str_replace($badword,'***',$paragrafo);
+echo $new_paragrafo;
+
+var_dump($new_paragrafo);
+echo strlen($new_paragrafo)
 
 ?>
